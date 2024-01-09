21dd57496055dcce4dbd6bb271202f594bd97c23
Raj Kasaudhan <rajksd9@gmail.com>
2024-01-09 11:30:38 +0530

    aeroplane migration updated and error object initialized

--- src/middlewares/aeroplane-middleware.js
+++ src/middlewares/aeroplane-middleware.js
@@ -1,4 +1,12 @@
-const validAeroplaneInput = (req,res,next) => { 
-    if(req.body)
-     
-}
+const { StatusCodes } = require("http-status-codes");
+const { errorResponse, error } = require("../utils/common/error-response");
+
+const validAeroplaneInput = (req, res, next) => {
+  if (!req.body.modelNumber) {
+    errorResponse.message = "ModelNumber is required ";
+    return res.status(StatusCodes.BAD_REQUEST).json(errorResponse);
+  }
+  next();
+};
+
+module.exports = validAeroplaneInput;
