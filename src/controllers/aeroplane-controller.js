7d72f946abd9d21576c7e86035c1cdeda9e2acfd
Raj Kasaudhan <rajksd9@gmail.com>
2024-01-10 22:15:34 +0530

    CRUD on aeroplane

--- src/controllers/aeroplane-controller.js
+++ src/controllers/aeroplane-controller.js
@@ -2,6 +2,8 @@ const { StatusCodes } = require("http-status-codes");
 const { AeroplaneServices } = require("../services");
 const { ErrorResponse, SuccessResponse } = require("../utils/common");
 
+// create a new Aeroplane
+
 async function createAeroplane(req, res) {
   try {
     const aeroplane = await AeroplaneServices.createAeroplane({
@@ -12,7 +14,6 @@ async function createAeroplane(req, res) {
     SuccessResponse.data = aeroplane;
     return res.status(StatusCodes.CREATED).json(SuccessResponse);
   } catch (error) {
-    
     ErrorResponse.error = error;
     return res.status(error.statusCode).json(ErrorResponse);
   }
@@ -22,25 +23,60 @@ async function createAeroplane(req, res) {
 
 async function deleteAeroplane(req, res) {
   try {
-    const aeroplane = await AeroplaneServices.deleteAeroplane(req.body.id);
-
-    return res.status(StatusCodes.OK).json({
-      success: true,
-      message: "Airplane deleted successfully",
-      data: aeroplane,
-      error: {},
-    });
+    await AeroplaneServices.deleteAeroplane(req.params.id);
+    SuccessResponse.data = `Aeroplane with id ${req.params.id} was deleted`;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
   } catch (error) {
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      success: false,
-      message: "Airplane deletion failed",
-      data: {},
-      error: error,
-    });
+    ErrorResponse.error = error;
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
+}
+
+// fetch all aeroplanes
+async function getAllAeroplane(req, res) {
+  try {
+    const aeroplanes = await AeroplaneServices.getAllAeroplane();
+    SuccessResponse.data = aeroplanes;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
+}
+
+// fetch a specific aeroplane from id
+
+async function getAeroplane(req, res) {
+  try {
+    const id = req.params.id;
+    const aeroplane = await AeroplaneServices.getAeroplane(id);
+    SuccessResponse.data = aeroplane;
+    return res.status(aeroplane.statusCode).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
+}
+// to update data of a aeroplane
+async function updateAeroplane(req, res) {
+  try {
+    const id = req.params.id;
+
+    await AeroplaneServices.updateAeroplane(id, req.body);
+    const updatedAeroplane = await AeroplaneServices.getAeroplane(id);
+    SuccessResponse.data = updatedAeroplane;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    console.log(error);
+    ErrorResponse.error = error;
+    return res.status(StatusCodes.SERVICE_UNAVAILABLE).json(ErrorResponse);
   }
 }
 
 module.exports = {
   createAeroplane,
   deleteAeroplane,
+  getAllAeroplane,
+  getAeroplane,
+  updateAeroplane,
 };
