0c7fe1eb21f2975a8f06869c9c279a6b34b30fed
Raj Kasaudhan <rajksd9@gmail.com>
2024-01-14 23:05:27 +0530

    added constraints on app level

--- src/models/city.js
+++ src/models/city.js
@@ -9,6 +9,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.hasOne(models.Airport, {
+        foreignKey: "cityId",
+      });
     }
   }
   City.init(
